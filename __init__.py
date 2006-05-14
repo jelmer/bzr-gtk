387a70439cf3e94928c959959072122d5c9db0e0
unknown <jbailey@ubuntu.com>
2006-05-13 22:15:35 -0400

    Bring this to a state where it actually works.  Specifically:
     * Add missing callback methods
     * Do not attempt to work on non-local URIs
     * Feed the URI, not a NautilusVFSFile to bzrlib
     * Feed the relative path to file_class
     * Put our menu items in a private BzrNautilus namespace
     * Nautilus items are done per-file, keep them within the scope of the loop.
    
    And also remove some unnecessary variable wrangling.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import nautilus
 import bzrlib
 from bzrlib.bzrdir import BzrDir
+from bzrlib.errors import NotBranchError
+from bzrlib.workingtree import WorkingTree
 
 class BzrExtension(nautilus.MenuProvider):
     def __init__(self):
@@ -9,62 +11,72 @@ class BzrExtension(nautilus.MenuProvider):
     def add_cb(self, menu, files):
         return
 
-    def get_file_items(self, window, files):
-        trees = []
-        items = []
-        file_class = None
+    def ignore_cb(self, menu, files):
+        return
 
-        for file in files:
+    def unignore_cb(self, menu, files):
+        return
+
+    def log_cb(self, menu, files):
+        return
+
+    def diff_cb(self, menu, files):
+        return
+
+    def remove_cb(self, menu, files):
+        return
+
+    def get_file_items(self, window, files):
+        items = []
+
+        for vfs_file in files:
+            # We can only cope with local files
+            if vfs_file.get_uri_scheme() != 'file':
+                return
+
+            file = vfs_file.get_uri()
             try:
-                dir = BzrDir.open_containing(file)
+                tree, path = WorkingTree.open_containing(file)
             except NotBranchError:
                 return
 
-            tree = dir.open_workingtree()
+            file_class = tree.file_class(path)
 
-            trees.append(tree)
+            if file_class == '?':
+                item = nautilus.MenuItem('BzrNautilus::add',
+                                     'Add',
+                                     'Add as versioned file')
+                item.connect('activate', self.add_cb, vfs_file)
+                items.append(item)
 
-            fc = tree.file_class(file)
-            if not file_class:
-                file_class = fc
-            elif fc != file_class:
-                return
+                item = nautilus.MenuItem('BzrNautilus::ignore',
+                                     'Ignore',
+                                     'Ignore file for versioning')
+                item.connect('activate', self.ignore_cb, vfs_file)
+                items.append(item)
+            elif file_class == 'I':
+                item = nautilus.MenuItem('BzrNautilus::unignore',
+                                     'Unignore',
+                                     'Unignore file for versioning')
+                item.connect('activate', self.unignore_cb, vfs_file)
+                items.append(item)
+            elif file_class == 'V':
+                item = nautilus.MenuItem('BzrNautilus::log',
+                                     'Log',
+                                     'List changes')
+                item.connect('activate', self.log_cb, vfs_file)
+                items.append(item)
 
-        if file_class == '?':
-            item = nautilus.MenuItem('Nautilus::add',
-                                 'Add',
-                                 'Add as versioned file')
-            item.connect('activate', self.add_cb, files)
-            items.append(item)
+                item = nautilus.MenuItem('BzrNautilus::diff',
+                                     'Diff',
+                                     'Show differences')
+                item.connect('activate', self.diff_cb, vfs_file)
+                items.append(item)
 
-            item = nautilus.MenuItem('Nautilus::ignore',
-                                 'Ignore',
-                                 'Ignore file for versioning')
-            item.connect('activate', self.ignore_cb, files)
-            items.append(item)
-        elif file_class == 'I':
-            item = nautilus.MenuItem('Nautilus::unignore',
-                                 'Unignore',
-                                 'Unignore file for versioning')
-            item.connect('activate', self.unignore_cb, files)
-            items.append(item)
-        elif file_class == 'V':
-            item = nautilus.MenuItem('Nautilus::log',
-                                 'Log',
-                                 'List changes')
-            item.connect('activate', self.log_cb, files)
-            items.append(item)
-
-            item = nautilus.MenuItem('Nautilus::diff',
-                                 'Diff',
-                                 'Show differences')
-            item.connect('activate', self.diff_cb, files)
-            items.append(item)
-
-            item = nautilus.MenuItem('Nautilus::remove',
-                                 'Remove',
-                                 'Remove this file from versioning')
-            item.connect('activate', self.remove_cb, files)
-            items.append(item)
-
+                item = nautilus.MenuItem('BzrNautilus::remove',
+                                     'Remove',
+                                     'Remove this file from versioning')
+                item.connect('activate', self.remove_cb, vfs_file)
+                items.append(item)
+    
         return items
