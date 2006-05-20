a6c28d863a7a450f3accfdd31c46d39dc2c06c73
Jelmer Vernooij <jelmer@samba.org>
2006-05-20 21:23:38 +0200

    Add framework for "clone branch" dialog

---
+++ clone.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2006 Jelmer Vernooij <jelmer@samba.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import pygtk
+pygtk.require("2.0")
+import gobject
+import gtk
+import pango
+from bzrlib.delta import compare_trees
+
+class GCloneDialog(gtk.Dialog):
+    """ Branch (clone) Dialog """
+
+    def __init__(self, dest_path=None):
+        gtk.Dialog.__init__(self)
+
+        self.set_default_size(400, 200)
+
+        self._create()
+
+    def _create(self):
+        self.add_buttons(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, "_Get", gtk.RESPONSE_OK)
+                         
