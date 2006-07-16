6ca5c88276e91e8d772ace6a64d50c9f96a6484e
Szilveszter Farkas (Phanatic) <Szilveszter.Farkas@gmail.com>
2006-07-16 18:27:59 +0200

    2006-07-16  Szilveszter Farkas <Szilveszter.Farkas@gmail.com>
        * Done some directory reorganization.
        * setup.py: added some basic install script
        * Began to implement the GTK UI.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2006 by Szilveszter Farkas (Phanatic) <szilveszter.farkas@gmail.com>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from distutils.core import setup
+
+setup(name='Olive',
+      version='0.1',
+      description='Olive - Graphical frontend for Bazaar-NG',
+      author='Szilveszter Farkas (Phanatic)',
+      author_email='szilveszter.farkas@gmail.com',
+      url='https://launchpad.net/products/olive/',
+      packages=['olive.backend', 'olive.frontend.gtk'],
+      package_dir={'olive.backend': 'olive/backend', 'olive.frontend.gtk': 'olive/frontend/gtk'})
