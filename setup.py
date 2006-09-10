7288decc8b0b8f07c4fe46171201a20764b50dce
Szilveszter Farkas (Phanatic) <Szilveszter.Farkas@gmail.com>
2006-09-10 23:14:28 +0200

    Fix the Win32 build/install issue.

--- setup.py
+++ setup.py
@@ -20,8 +20,9 @@ from distutils.core import setup
 from distutils.command.install_data import install_data
 from distutils.dep_util import newer
 from distutils.log import info
-import os
 import glob
+import os
+import sys
 
 class InstallData(install_data):
 	def run(self):
@@ -30,6 +31,11 @@ class InstallData(install_data):
 
 	def _compile_po_files(self):
 		data_files = []
+		
+		# Don't install language files on Win32
+		if sys.platform == 'win32':
+		    return data_files
+		
 		PO_DIR = 'po'
 		for po in glob.glob(os.path.join(PO_DIR,'*.po')):
 			lang = os.path.basename(po[:-3])
