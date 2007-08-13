82a40cd241ddca5d4e45606844a771c4ffc17132
Szilveszter Farkas (Phanatic) <Szilveszter.Farkas@gmail.com>
2007-08-13 12:08:10 +0200

    Updated POT generator script and also regenerated the translation template.

--- genpot.sh
+++ genpot.sh
@@ -20,10 +20,13 @@
 
 if [ -x po/olive-gtk.pot ]; then
     rm po/olive-gtk.pot
+    touch po/olive-gtk.pot
+else
+    touch po/olive-gtk.pot
 fi
 xgettext -L python -o po/olive-gtk.pot olive-gtk
 xgettext -j -o po/olive-gtk.pot olive.glade
-xgettext -o po/olive-gtk.pot *.py
+xgettext -j -o po/olive-gtk.pot *.py
 cd olive/
 xgettext -j -o ../po/olive-gtk.pot *.py
 #cd viz
