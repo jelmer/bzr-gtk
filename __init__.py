349a733cf5d8fae8bbc29654528345cab69c8864
Jelmer Vernooij <jelmer@samba.org>
2007-08-27 14:38:18 +0200

    Add common widget for selecting branches.

--- __init__.py
+++ __init__.py
@@ -57,7 +57,7 @@ def check_bzrlib_version(desired):
         from warnings import warn as warning
     if bzrlib_version < desired:
         from bzrlib.errors import BzrError
-        warning('Installed bzr version %s is too old to be used with bzr-gtk'
+        warning('Installed Bazaar version %s is too old to be used with bzr-gtk'
                 ' %s.' % (bzrlib.__version__, __version__))
         raise BzrError('Version mismatch: %r' % (version_info,) )
     else:
