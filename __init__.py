b6e825249c61ffe2d893d5edbdd3e33af7183162
Szilveszter Farkas (Phanatic) <Szilveszter.Farkas@gmail.com>
2007-09-24 21:06:13 +0200

    Start working on 0.92.0

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ visualise         Graphically visualise this branch.
 
 import bzrlib
 
-__version__ = '0.91.0'
+__version__ = '0.92.0.dev'
 version_info = tuple(int(n) for n in __version__.split('.'))
 
 
