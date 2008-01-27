15e1d65b861c9da07b568af80262687bdb0790b7
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-01-27 02:13:38 +0100

    Changed About dialog URL, reverted change to license dialog.

--- about.py
+++ about.py
@@ -30,8 +30,8 @@ class AboutDialog(gtk.AboutDialog):
         super(AboutDialog, self).__init__()
         self.set_name("Bazaar GTK")
         self.set_version(bzrlib.plugins.gtk.version_string)
-        self.set_website("http://bazaar-vcs.org/")
-        self.set_license(open("COPYING", "r").read())
+        self.set_website("http://bazaar-vcs.org/BzrGtk")
+        self.set_license("GNU GPL v2")
         self.set_icon(gtk.gdk.pixbuf_new_from_file(os.path.join(data_path(), "bzr-icon-64.png")))
         self.connect ("response", lambda d, r: d.destroy())
 
