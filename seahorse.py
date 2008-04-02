7d7c065addfeb05002e35a45ecbde4e1251213ef
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-04-03 01:39:44 +0200

    Fixed encoding error.

--- seahorse.py
+++ seahorse.py
@@ -12,7 +12,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-__copyright__ = 'Copyright © 2008 Daniel Schierbeck'
+__copyright__ = 'Copyright (C) 2008 Daniel Schierbeck'
 __author__ = 'Daniel Schierbeck <daniel.schierbeck@gmail.com>'
 
 import dbus
