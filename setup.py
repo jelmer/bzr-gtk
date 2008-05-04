c0054f837d902aefb71450431418955cb5254ac1
Jelmer Vernooij <jelmer@samba.org>
2008-05-04 20:36:55 +0200

    Release 0.94.0.

--- setup.py
+++ setup.py
@@ -91,11 +91,11 @@ class InstallData(install_data):
 
 setup(
     name = "bzr-gtk",
-    version = "0.94.0rc1",
+    version = "0.94.0",
     maintainer = "Jelmer Vernooij",
     maintainer_email = "jelmer@samba.org",
     description = "GTK+ Frontends for various Bazaar commands",
-    license = "GNU GPL v2",
+    license = "GNU GPL v2 or later",
     scripts=['olive-gtk'],
     package_dir = {
         "bzrlib.plugins.gtk": ".",
