f87ba0ca9870c9a3394c236759380c8ca95c68a7
Jelmer Vernooij <jelmer@samba.org>
2008-05-04 20:28:46 +0200

    Disable signature tab until we actually start verifying the testament.

--- revisionview.py
+++ revisionview.py
@@ -317,8 +317,9 @@ class RevisionView(gtk.Notebook):
 
         self._create_general()
         self._create_relations()
-        if has_seahorse:
-            self._create_signature()
+        # Disabled because testaments aren't verified yet:
+        # if has_seahorse:
+        #    self._create_signature()
         self._create_file_info_view()
         self._create_bugs()
 
