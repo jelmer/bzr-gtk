acf682baddbcc97fbb60621c7fff40b1c1d4b899
klight <klight@X111>
2008-07-14 14:48:05 -0400

    This patch fixes a traceback when the olive rename dialog is to be displayed. The wrong import location was called for show_bzr_error.

--- olive/rename.py
+++ olive/rename.py
@@ -28,7 +28,7 @@ import gtk.glade
 import bzrlib.errors as errors
 from bzrlib.workingtree import WorkingTree
 
-from errors import show_bzr_error
+from bzrlib.plugins.gtk.errors import show_bzr_error
 from bzrlib.plugins.gtk.dialog import error_dialog
 from guifiles import GLADEFILENAME
 from bzrlib.plugins.gtk import _i18n
