a0e84e9237bbba138b2fab2ef99f926d35587902
Jelmer Vernooij <jelmer@samba.org>
2008-10-30 14:48:44 +0100

    Merge trunk, add cia/email fields.

--- preferences/notifications.py
+++ preferences/notifications.py
@@ -22,18 +22,58 @@ except:
 
 import gtk
 
+import bzrlib.plugins.gtk
 from bzrlib.plugins.gtk.notify import has_avahi, has_dbus
+
+def has_email():
+    return (getattr(bzrlib.plugins, "email", None) is not None)
+
+def has_cia():
+    return (getattr(bzrlib.plugins, "cia", None) is not None)
+
 
 class NotificationsPage(gtk.VBox):
     def __init__(self, config, homogeneous=False, spacing=6):
         self.config = config
         gtk.VBox.__init__(self, homogeneous=homogeneous, spacing=spacing)
         self.set_spacing(spacing) # The vertical one
+        
+        lan_frame = gtk.Frame("LAN Notifications")
+
+        lan_vbox = gtk.VBox()
+        lan_frame.add(lan_vbox)
 
         self.gateway_to_lan = gtk.CheckButton("_Gateway to LAN")
-        self.pack_start(self.gateway_to_lan, expand=False)
+        lan_vbox.pack_start(self.gateway_to_lan)
         self.gateway_to_lan.set_sensitive(has_dbus())
 
         self.announce_on_lan = gtk.CheckButton("_Announce on LAN")
-        self.pack_start(self.announce_on_lan, expand=False)
+        lan_vbox.pack_start(self.announce_on_lan)
         self.announce_on_lan.set_sensitive(has_avahi())
+
+        self.pack_start(lan_frame)
+
+        email_frame = gtk.Frame("E-mail notifications")
+
+        email_hbox = gtk.HBox()
+        self.send_email = gtk.CheckButton("Send _E-Mail to")
+        email_hbox.pack_start(self.send_email)
+        self.send_email_to = gtk.Entry()
+        email_hbox.pack_start(self.send_email_to)
+
+        email_frame.add(email_hbox)
+        email_frame.set_sensitive(has_email())
+
+        self.pack_start(email_frame)
+
+        cia_frame = gtk.Frame("CIA notifications")
+        
+        cia_user_hbox = gtk.HBox()
+        cia_user_hbox.pack_start(gtk.Label("Author name"))
+        self.cia_user = gtk.Entry()
+        cia_user_hbox.pack_start(self.cia_user)
+    
+        cia_frame.add(cia_user_hbox)
+        cia_frame.set_sensitive(has_cia())
+
+        self.pack_start(cia_frame)
