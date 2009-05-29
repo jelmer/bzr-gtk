5263e1893eb2fcf5065f79f682eee7cd679be153
Mario Danic <mario.danic@gmail.com>
2009-05-29 10:28:10 +0200

    Fix wrongly placed import os

--- commands.py
+++ commands.py
@@ -12,6 +12,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+import os
 from bzrlib import (
     branch,
     builtins,
@@ -267,7 +268,6 @@ class cmd_gcommit(GTKCommand):
     takes_options = []
 
     def run(self, filename=None):
-        import os
         open_display()
         from commit import CommitDialog
         from bzrlib.errors import (BzrCommandError,
