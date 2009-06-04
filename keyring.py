49dad7d8c731e289ab1e265383730c1b944caa88
Jelmer Vernooij <jelmer@samba.org>
2009-06-04 23:10:21 +0200

    Catch NoKeyringDaemonError.

--- keyring.py
+++ keyring.py
@@ -73,5 +73,5 @@ class GnomeKeyringCredentialStore(CredentialStore):
                 gnomekeyring.ITEM_NETWORK_PASSWORD, attrs)
             credentials["user"] = items[0].attributes["user"]
             credentials["password"] = items[0].secret
-        except (gnomekeyring.NoMatchError, gnomekeyring.DeniedError):
+        except (gnomekeyring.NoMatchError, gnomekeyring.DeniedError, gnomekeyring.NoKeyringDaemonError):
             return None
