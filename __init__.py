e70df2047520413b537f9b72dcaa8ec98ad8ec4f
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2009-08-25 15:08:27 +0200

    Revert previous spurious commit.
    * __init__.py:
    (COMPATIBLE_BZR_VERSIONS): Damn, tricked by a buggy report and the
    variable name, this really is the bzrli API versions, not the
    official bzr versions.

--- __init__.py
+++ __init__.py
@@ -57,8 +57,6 @@ COMPATIBLE_BZR_VERSIONS = [(1, 6, 0), (1, 7, 0), (1, 8, 0), (1, 9, 0),
                            (1, 10, 0), (1, 11, 0), (1, 12, 0), (1, 13, 0),
                            (1, 15, 0),
                            (1, 17, 0),
-                           (1, 18, 0),
-                           (2, 0 ,0),
                            ]
 
 bzrlib.api.require_any_api(bzrlib, COMPATIBLE_BZR_VERSIONS)
