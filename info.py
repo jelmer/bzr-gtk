302f97fbe903403e79c5c8e46cdcf0e4eb252387
Jelmer Vernooij <jelmer@samba.org>
2011-01-16 19:55:13 +0100

    Mark as compatible with Bazaar 2.4.

--- info.py
+++ info.py
@@ -32,6 +32,7 @@ bzr_compatible_versions = [(1, 6, 0), (1, 7, 0), (1, 8, 0), (1, 9, 0),
                            (2, 1, 0),
                            (2, 2, 0),
                            (2, 3, 0),
+                           (2, 4, 0),
                            ]
 
 bzr_minimum_version = bzr_compatible_versions[0]
