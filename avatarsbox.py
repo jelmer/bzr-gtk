5dc99cc23b14daec379ad392130e92ea9dec560a
zedtux <zedtux@zedroot.org>
2011-03-15 17:46:35 +0100

    Moved Avatar and AvatarBox classes into avatarsbox file

--- avatarsbox.py
+++ avatarsbox.py
@@ -16,10 +16,151 @@
 
 import gtk
 
-from bzrlib.plugins.gtk.avatar import Avatar
-from bzrlib.plugins.gtk.avatarbox import AvatarBox
+from bzrlib.plugins.gtk import _i18n
+from bzrlib.config import parse_username
 from bzrlib.plugins.gtk.avatarproviders import AvatarProviderGravatar
 from bzrlib.plugins.gtk.avatardownloaderworker import AvatarDownloaderWorker
+
+
+class Avatar(gtk.Box):
+    """ Author or committer avatar """
+    
+    def __init__(self, apparent_username):
+        """ Constructor """
+        gtk.Box.__init__(self)
+        
+        self.apparent_username = apparent_username
+        self.username, self.email = parse_username(apparent_username)
+        self.image = None
+    
+    def __eq__(self, other):
+        """
+        Return True if attributes of the given avatar
+        match to current object attributes otherwise return False
+        """
+        return self.apparent_username == other.apparent_username and \
+               self.name == other.name and \
+               self.email == other.email
+    
+    # ~~~~~ Public methods ~~~~~
+    def show_spinner(self):
+        """
+        Replace the current content of the Avatar with a gtk.Spinner
+        if an email address has been parsed. If not, show an gtk.Label with
+        the translatable 'No email' text.
+        """
+        if not self.email is "":
+            spinner = gtk.Spinner()
+            spinner.start()
+            self.pack_start(spinner, False)
+            spinner.set_tooltip_text(_i18n("Retrieving avatar for %s...") % self.email)
+            spinner.set_size_request(20, 20)
+            spinner.show()
+        else:
+            no_email = gtk.Label(_i18n("No email"))
+            self.pack_start(no_email)
+            self.set_tooltip_text(self.apparent_username)
+            no_email.show()
+    
+    def show_image(self):
+        """ Replace the current content of the Avatar with the gtk.Image """
+        if not self.email is "" and self.image:
+            self.remove(self.get_children()[0])
+            self.pack_start(self.image)
+            self.image.set_tooltip_text(self.apparent_username)
+            self.image.show()
+
+
+class AvatarBox(gtk.HBox):
+    """ Improved gtk.HBox """
+    
+    def __init__(self, homogeneous=False, spacing=0):
+        """ Constructor """
+        gtk.HBox.__init__(self, homogeneous, spacing)
+        self.__avatars = {}
+        self.avatar = None
+        self.__displaying = None
+    
+    
+    # ~~~~~ Public methods ~~~~~
+    def reset_view(self):
+        """ Remove current avatars from the gtk box """
+        for child in self.get_children():
+            self.remove(child)
+        self.__displaying = None
+    
+    def have_avatar(self, avatar):
+        """
+        Return True if this box has registered given avatar,
+        otherwise return False
+        """
+        return avatar.email in self.__avatars
+    
+    def showing(self, avatar):
+        """
+        Return True if the displaying avatar is the same
+        as the given one otherwise return False
+        """
+        return self.__displaying and self.__displaying == avatar
+    
+    def append_avatars_with(self, avatar):
+        """
+        Append avatars collection with the given one if not already registed
+        otherwise render it back.
+        When an avatar is added this method True, otherwise, if the avatar
+        was in the collection, return False.
+        """
+        if not avatar.email is "" and not avatar.email in self.__avatars:
+            self.__avatars[avatar.email] = avatar
+            self._new_cell_for_avatar(avatar)
+            return True
+        else:
+            self.and_avatar_email(avatar.email).come_back_to_gui()
+        return False
+    
+    def and_avatar_email(self, email):
+        """
+        Select the avatar from avatars collection
+        in order to apply future actions
+        """
+        self.avatar = None
+        if not email is "" and email in self.__avatars:
+            self.avatar = self.__avatars[email]
+        else:
+            self.avatar = None
+        return self
+    
+    def update_avatar_image_from_pixbuf_loader(self, loader):
+        """ Replace the gtk.Spinner with the given loader """
+        if self.avatar:
+            self.avatar.image = gtk.Image()
+            self.avatar.image.set_from_pixbuf(loader.get_pixbuf())
+            self.avatar.show_image()
+            self.__displaying = self.avatar
+    
+    def come_back_to_gui(self):
+        """ Render back avatar in the GUI """
+        if self.avatar:
+            self.pack_start(self.avatar)
+            self.__displaying = self.avatar
+        else:
+            self._show_no_email()
+    
+    
+    # ~~~~~ Private methods ~~~~~~
+    def _new_cell_for_avatar(self, avatar):
+        """ Create a new cell in this box with a gtk.Spinner """
+        avatar.show_spinner()
+        self.pack_start(avatar)
+        avatar.show()
+        self.__displaying = avatar
+    
+    def _show_no_email(self):
+        """ Show a gtk.Label with test 'No email' """
+        no_email = gtk.Label(_i18n("No email"))
+        self.pack_start(no_email)
+        no_email.show()
+
 
 class AvatarsBox(gtk.HBox):
     """ GTK container for authors and committers avatars """
