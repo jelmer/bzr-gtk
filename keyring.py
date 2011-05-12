8e4596c7e025b612bc5ac916ddabc0616ad17b9b
Chris Causer <chy.causer@gmail.com>
2011-05-12 18:16:34 +0100

    Fix keyring problem with svn and https

--- keyring.py
+++ keyring.py
@@ -57,29 +57,34 @@ class GnomeKeyringCredentialStore(CredentialStore):
         except (gnomekeyring.NoMatchError, gnomekeyring.DeniedError):
             return None
 
-    def get_credentials(self, scheme, host, port=None, user=None, path=None, 
+    def get_credentials(self, scheme, host, port=None, user=None, path=None,
                         realm=None):
         if gnomekeyring is None:
             return None
-        attrs = {
-            "protocol": scheme.encode("utf-8"),
-            "server": host.encode("utf-8"),
-            }
-        # TODO: realm ?
+        if scheme in ('http', 'https'):
+            attrs = {'domain': realm}
+        else:
+            attrs = {
+                "protocol": scheme.encode("utf-8"),
+                "server": host.encode("utf-8"),
+                }
+        # TODO: realm for non http, https?
         if port is not None:
             attrs["port"] = port
         if user is not None:
             attrs["user"] = user.encode("utf-8")
-        credentials = { "scheme": scheme, "host": host, "port": port, 
+        attrs["domain"] = realm
+        credentials = {"scheme": scheme, "host": host, "port": port,
             "realm": realm, "user": user}
+        print locals()
         try:
             items = gnomekeyring.find_items_sync(
                 gnomekeyring.ITEM_NETWORK_PASSWORD, attrs)
             credentials["user"] = items[0].attributes["user"]
             credentials["password"] = items[0].secret
             return credentials
-        except (gnomekeyring.NoMatchError, gnomekeyring.DeniedError, gnomekeyring.NoKeyringDaemonError,
-                gnomekeyring.IOError), e:
+        except (gnomekeyring.NoMatchError, gnomekeyring.DeniedError,
+                gnomekeyring.NoKeyringDaemonError, gnomekeyring.IOError), e:
             from bzrlib import trace
             trace.mutter('Unable to obtain credentials for %r from GNOME keyring: %r',
                          attrs, e)
