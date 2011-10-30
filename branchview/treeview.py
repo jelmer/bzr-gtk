bc2bf9b4ad6ef929da8f5313c55d41e4bf320288
Jelmer Vernooij <jelmer@samba.org>
2011-10-30 01:33:37 -0700

    Fix number of arguments to gtk.Menu.popup.

--- branchview/treeview.py
+++ branchview/treeview.py
@@ -458,7 +458,7 @@ class TreeView(Gtk.VBox):
                 [self.get_revision().revision_id],
                 self.branch)
             menu.connect('tag-added', lambda w, t, r: self.add_tag(t, r))
-            menu.popup(None, None, None, event.button, event.get_time())
+            menu.popup(None, None, None, None, event.button, event.get_time())
 
     def _on_revision_activated(self, widget, path, col):
         self.emit('revision-activated', path, col)
