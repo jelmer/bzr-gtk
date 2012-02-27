4a89b8fc38f1070305e2eeb267bace25f40b6bfe
Curtis Hovey <sinzui.is@verizon.net>
2012-02-27 17:54:10 -0500

    Merged previous UI work from progressbar.

--- push.py
+++ push.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2006 by Szilveszter Farkas (Phanatic) <szilveszter.farkas@gmail.com>
+# Copyright (C) 2006 by Szilveszter Farkas (Phanatic)
+#                       <szilveszter.farkas@gmail.com>
 # Copyright (C) 2007 by Jelmer Vernooij <jelmer@samba.org>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -15,20 +16,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from gi.repository import GObject
 from gi.repository import Gtk
 
 from errors import show_bzr_error
 
-import bzrlib.errors as errors
+from bzrlib import (
+    errors,
+    ui,
+    )
+from bzrlib.bzrdir import BzrDir
+from bzrlib.transport import get_transport
 
 from bzrlib.plugins.gtk.dialog import (
-    info_dialog,
     question_dialog,
     )
-
 from bzrlib.plugins.gtk.history import UrlHistory
 from bzrlib.plugins.gtk.i18n import _i18n
+from bzrlib.plugins.gtk.ui import ProgressPanel
 
 
 class PushDialog(Gtk.Dialog):
@@ -38,8 +42,7 @@ class PushDialog(Gtk.Dialog):
         """Initialize the Push dialog. """
         super(PushDialog, self).__init__(
             title="Push", parent=parent, flags=0,
-            buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-
+            buttons=(Gtk.STOCK_CLOSE, Gtk.ResponseType.CLOSE))
 
         # Get arguments
         self.repository = repository
@@ -53,6 +56,7 @@ class PushDialog(Gtk.Dialog):
         self._hbox_location = Gtk.HBox()
 
         # Set callbacks
+        self.connect('close', self._on_close_clicked)
         self._button_push.connect('clicked', self._on_push_clicked)
 
         # Set properties
@@ -63,13 +67,24 @@ class PushDialog(Gtk.Dialog):
         # Pack widgets
         self._hbox_location.pack_start(
             self._label_location, False, False, 0)
-        self._hbox_location.pack_start(self._combo, True, True, 0)
-        self.get_content_area().pack_start(self._hbox_location, True, True, 0)
-        # XXX sinzui 2011-08-12: maybe False, False, 0
+        self._hbox_location.pack_start(self._combo, False, False, 0)
+        self.get_content_area().pack_start(
+            self._hbox_location, False, False, 0)
         self.get_action_area().pack_end(self._button_push, True, True, 0)
+
+        # Set progress pane.
+        self.progress_widget = ProgressPanel()
+        ui.ui_factory.set_progress_bar_widget(self.progress_widget)
+        self.get_content_area().pack_start(
+            self.progress_widget, False, False, 0)
+        self.push_message = Gtk.Label()
+        alignment = Gtk.Alignment.new(0.0, 0.5, 0.0, 0.0)
+        alignment.add(self.push_message)
+        self.get_content_area().pack_start(alignment, False, False, 0)
 
         # Show the dialog
         self.get_content_area().show_all()
+        self.progress_widget.hide()
 
         # Build location history
         self._history = UrlHistory(self.branch.get_config(), 'push_history')
@@ -79,7 +94,7 @@ class PushDialog(Gtk.Dialog):
         """Build up the location history. """
         self._combo_model = Gtk.ListStore(str)
         for item in self._history.get_entries():
-            self._combo_model.append([ item ])
+            self._combo_model.append([item])
         self._combo.set_model(self._combo_model)
         self._combo.set_entry_text_column(0)
 
@@ -88,28 +103,38 @@ class PushDialog(Gtk.Dialog):
             if location is not None:
                 self._combo.get_child().set_text(location)
 
+    def _on_close_clicked(self, widget):
+        """Close dialog handler."""
+        ui.ui_factory.set_progress_bar_widget(None)
+
     @show_bzr_error
     def _on_push_clicked(self, widget):
         """Push button clicked handler. """
+        self.push_message.hide()
+        self.progress_widget.tick()
         location = self._combo.get_child().get_text()
-        revs = 0
 
         try:
-            revs = do_push(self.branch, location=location, overwrite=False)
+            message = do_push(self.branch, location=location, overwrite=False)
         except errors.DivergedBranches:
-            response = question_dialog(_i18n('Branches have been diverged'),
-                                       _i18n('You cannot push if branches have diverged.\nOverwrite?'))
+            response = question_dialog(
+                _i18n('Branches have been diverged'),
+                _i18n('You cannot push if branches have diverged.\n'
+                      'Overwrite?'))
             if response == Gtk.ResponseType.YES:
-                revs = do_push(self.branch, location=location, overwrite=True)
-
-        if self.branch is not None and self.branch.get_push_location() is None:
-            self.branch.set_push_location(location)
-
+                message = do_push(
+                    self.branch, location=location, overwrite=True)
+            else:
+                return
         self._history.add_entry(location)
-        info_dialog(_i18n('Push successful'),
-                    _i18n("%d revision(s) pushed.") % revs)
-
-        self.response(Gtk.ResponseType.OK)
+        if (self.branch is not None
+            and self.branch.get_push_location() is None):
+            self.branch.set_push_location(location)
+        if message:
+            self.progress_widget.finished()
+            self.push_message.props.label = message
+            self.push_message.show()
+        message
 
 
 def do_push(br_from, location, overwrite):
@@ -120,13 +145,8 @@ def do_push(br_from, location, overwrite):
     :param overwrite: overwrite target location if it diverged
     :return: number of revisions pushed
     """
-    from bzrlib.bzrdir import BzrDir
-    from bzrlib.transport import get_transport
-
     transport = get_transport(location)
     location_url = transport.base
-
-    old_rh = []
 
     try:
         dir_to = BzrDir.open(location_url)
@@ -138,8 +158,10 @@ def do_push(br_from, location, overwrite):
             relurl = transport.relpath(location_url)
             transport.mkdir(relurl)
         except errors.NoSuchFile:
-            response = question_dialog(_i18n('Non existing parent directory'),
-                         _i18n("The parent directory (%s)\ndoesn't exist. Create?") % location)
+            response = question_dialog(
+                _i18n('Non existing parent directory'),
+                _i18n("The parent directory (%s)\ndoesn't exist. Create?") %
+                location)
             if response == Gtk.ResponseType.OK:
                 transport.create_prefix()
             else:
@@ -149,7 +171,7 @@ def do_push(br_from, location, overwrite):
         br_to = dir_to.open_branch()
         count = len(br_to.revision_history())
     else:
-        old_rh = br_to.revision_history()
+        br_to.revision_history()
         try:
             tree_to = dir_to.open_workingtree()
         except errors.NotLocalUrl:
@@ -160,4 +182,4 @@ def do_push(br_from, location, overwrite):
         else:
             count = tree_to.pull(br_from, overwrite)
 
-    return count
+    return str(count)
